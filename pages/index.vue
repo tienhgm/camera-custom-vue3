e7ef3fe040263f77815d1a2f04ee48ee1ab60288
tiennguyen <tienhgm@gmail.com>
2023-06-13 22:52:14 +0700

    [Init vue3]

---
+++ pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    value: {{ refVal }}
+    <button @click="clickPlus">Plus</button>
+  </div>
+</template>
+<script>
+export default {
+  name: "Index",
+  setup() {
+    let refVal = ref(0);
+    let val1 = ref(1);
+    let val2 = ref(1);
+    refVal.value = val1.value + val2.value;
+    const clickPlus = () => {
+      val1.value = 2;
+    };
+    return {
+      refVal,
+      clickPlus,
+    };
+  },
+};
+</script>
+<style></style>
---
+++ pages/test.vue
@@ -0,0 +1,99 @@
+<template>
+  <div class="parent">
+    <video id="video" autoplay></video>
+    <canvas id="canvas"></canvas>
+    <div class="bottom-camera"></div>
+    <img src="~/assets/imgs/temp.png" class="img-temp" />
+    <div class="capture" id="snap" @click="onCapture()"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Test",
+  data() {
+    return {
+      video: "",
+    };
+  },
+  async mounted() {
+    try {
+      const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+      if (stream) this.handleSuccess(stream);
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  methods: {
+    handleSuccess(stream) {
+      const video = document.querySelector("#video");
+      video.srcObject = stream;
+    },
+    onCapture() {
+      const canvas = document.getElementById("canvas");
+      var ctx = canvas.getContext("2d");
+      const video = document.querySelector("#video");
+      ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+      const dataURL = canvas.toDataURL();
+      console.log(dataURL);
+    },
+  },
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+  padding: 0;
+  margin: 0;
+}
+.parent {
+  position: relative;
+  overflow: hidden;
+  width: 100vw;
+  height: 100vh;
+}
+#video {
+  width: 100%;
+  /* height: inherit; */
+}
+.img-temp {
+  position: absolute;
+  width: 50%;
+  height: 40%;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+}
+img {
+  display: block;
+  width: 100%;
+  height: 100%;
+}
+#canvas {
+  position: absolute;
+  z-index: 9;
+  width: 60px;
+  height: 60px;
+  object-fit: cover;
+  bottom: 2rem;
+  left: 5%;
+}
+.bottom-camera {
+  height: 18%;
+  position: absolute;
+  width: inherit;
+  bottom: 0;
+  background-color: rgba(145, 24, 24, 0.769);
+}
+.capture {
+  cursor: pointer;
+  bottom: 2rem;
+  left: 50%;
+  position: absolute;
+  background: #fff;
+  border-radius: 50%;
+  width: 60px;
+  height: 60px;
+}
+</style>
